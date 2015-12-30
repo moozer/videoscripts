287ed66dbe21c01d6b9be7bda3f0e0c85396fac2
moz <moz@local>
2015-12-30 22:22:57 +0100

    doc stuff var refactor

---
+++ convertVideoToh264.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# inspiration from :
+# http://stackoverflow.com/questions/28036830/how-to-use-gstreamer-for-transcoding-and-resizing-from-mp4h264-aac-to-mp4h264
+
+if [ -z "$1" ]; then
+	echo "usage"
+	echo "  $0 <filename>"
+	exit
+fi
+
+INNAME=$1
+OUTNAME="$INNAME.mp4"
+
+# h264 bitrate
+# http://gstreamer.freedesktop.org/data/doc/gstreamer/head/gst-plugins-ugly-plugins/html/gst-plugins-ugly-plugins-x264enc.html#GstX264Enc--bitrate
+BITRATE=5120
+# 5Mbit/s seems to be recommended
+
+# pipeline
+# decode whatever and send it to "bin"
+# encode audio to aac using voaacenc and send to outmux
+# encode video to h264 using x264enc and send to outmux
+
+gst-launch-1.0 filesrc location=$INNAME ! decodebin name=bin \
+  bin. ! progressreport \
+  ! videoscale ! x264enc bitrate=$BITRATE ! queue ! outmux. \
+  bin. ! audioconvert ! voaacenc ! outmux. \
+  mp4mux name=outmux ! filesink location=$OUTNAME
+  
+echo "Saved stream to $OUTNAME"
+  
+ 
