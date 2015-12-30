6bd1406693cbe8a6aac741ae7769e27e3a6966d8
moz <moz@local>
2015-12-30 23:08:03 +0100

    added debug concept to convertvideo

--- convertVideoToh264.sh
+++ convertVideoToh264.sh
@@ -5,8 +5,14 @@
 
 if [ -z "$1" ]; then
 	echo "usage"
-	echo "  $0 <filename>"
+	echo "  $0 <filename> <debug level>"
 	exit
+fi
+
+if [ -z "$2" ]; then
+	DEBUGLVL=0
+else
+	DEBUGLVL=$2
 fi
 
 INNAME=$1
@@ -18,15 +24,16 @@ BITRATE=5120
 # 5Mbit/s seems to be recommended
 
 # pipeline
-# decode whatever and send it to "bin"
-# encode audio to aac using voaacenc and send to outmux
-# encode video to h264 using x264enc and send to outmux
+#   decode whatever and send it to "bin"
+#   encode audio to aac using voaacenc and send to outmux
+#   encode video to h264 using x264enc and send to outmux
 
 gst-launch-1.0 filesrc location=$INNAME ! decodebin name=bin \
   bin. ! progressreport \
   ! videoscale ! x264enc bitrate=$BITRATE ! queue ! outmux. \
   bin. ! audioconvert ! voaacenc ! outmux. \
-  mp4mux name=outmux ! filesink location=$OUTNAME
+  mp4mux name=outmux ! filesink location=$OUTNAME \
+  --gst-debug-level=$DEBUGLVL
   
 echo "Saved stream to $OUTNAME"
   
